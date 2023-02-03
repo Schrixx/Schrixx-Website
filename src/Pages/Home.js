582b6c6459befeb901349beef17d7fabfdfd9653
Schrixx <fun_nos@yahoo.com>
2023-02-02 21:14:49 -0700

    feat: add text within styled containers
    Release-As: 0.2.0

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,20 +5,48 @@ import React from "react";
 import Navbar from "../Components/Navbar";
 import {
   InfoContainer,
-  AboutMeText,
-  General,
-  Hobbies,
+  GeneralContainer,
+  HobbiesContainer,
+  Line1,
+  FooterContainer,
 } from "../Styles/HomeStyled";
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      <AboutMeText>About me</AboutMeText>
+
       <InfoContainer>
-        <General></General>
-        <Hobbies></Hobbies>
+        <GeneralContainer>
+          <h1>General</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+            volutpat enim eget metus pellentesque consectetur et a felis. Donec
+            sollicitudin enim id imperdiet feugiat. Etiam a vehicula elit.
+            Praesent eros dolor, auctor et ultrices nec, hendrerit nec lacus.
+            Donec sollicitudin pulvinar dictum. Nulla facilisi. Vivamus quis
+            metus lobortis, varius felis et, ullamcorper orci. Maecenas blandit
+            massa interdum, pulvinar felis venenatis, volutpat ligula.
+          </p>
+        </GeneralContainer>
+        <Line1></Line1>
+        <HobbiesContainer>
+          <h1>Hobbies</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+            volutpat enim eget metus pellentesque consectetur et a felis. Donec
+            sollicitudin enim id imperdiet feugiat. Etiam a vehicula elit.
+            Praesent eros dolor, auctor et ultrices nec, hendrerit nec lacus.
+            Donec sollicitudin pulvinar dictum. Nulla facilisi. Vivamus quis
+            metus lobortis, varius felis et, ullamcorper orci. Maecenas blandit
+            massa interdum, pulvinar felis venenatis, volutpat ligula.
+          </p>
+        </HobbiesContainer>
       </InfoContainer>
+
+      <FooterContainer>
+        <h1>Hi</h1>
+      </FooterContainer>
     </>
   );
 }
