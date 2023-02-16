21681f82f966cfb7ea51b1fc36d71e5868cccaf2
Schrixx <fun_nos@yahoo.com>
2023-02-16 00:40:17 -0700

    feat: add portfolio page

---
+++ src/Styles/PortfolioStyled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const FooterContainer = styled.section`
+  display: none;
+  position: fixed;
+  bottom: 0;
+  background-color: black;
+  width: 100%;
+`;
